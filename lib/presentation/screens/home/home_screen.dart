80b3b9aec45e0d8d6980caf436bb8517e271c342
sergio <granrah1@gmail.com>
2023-04-21 13:03:30 -0400

    rutas

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/menu/menu_items.dart';
+import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -11,6 +13,55 @@ class HomeScreen extends StatelessWidget {
           'Flutter + Material 3',
         ),
       ),
+      body: const _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView();
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        final menuItem = appMenuItems[index];
+
+        // hice el snippet en la ruta command shift p usersnippets luego buscar dart
+        // y pegar el codigo
+
+        // "Color scheme": {
+        //         "prefix":"theme-of-context",
+        //         "body": [
+        //             "final colors = Theme.of(context).colorScheme;",
+        //         ],
+        //         "description":"Create the reference to the colors"
+
+        //     }
+
+        // por lo que escribo theme ofy me aparece el snippet
+        // luego accedo al color primario determinado por el color que maneja la app
+        // de mi lista e colores creada
+
+        final colors = Theme.of(context).colorScheme;
+
+        return ListTile(
+          title: Text(menuItem.title),
+          subtitle: Text(menuItem.subTitle),
+          leading: Icon(
+            menuItem.icon,
+            color: colors.primary,
+          ),
+          trailing: Icon(
+            Icons.arrow_forward_ios_rounded,
+            color: colors.primary,
+          ),
+          onTap: () {
+            Navigator.pushNamed(context, menuItem.link);
+          },
+        );
+      },
+      itemCount: appMenuItems.length,
     );
   }
 }
